4a32b062052ea622b15685865593efd390fe7e71
Leda <ledathemis@protonmail.com>
2022-04-06 13:26:48 +0300

    add form validation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Book {
   constructor(title, author, pages, isRead) {
-    this.title = title === "" ? "Unknown" : title;
-    this.author = author === "" ? "Unknown" : author;
-    this.pages = pages === "" ? "Unknown" : pages;
+    this.title = title === '' ? 'Unknown' : title;
+    this.author = author === '' ? 'Unknown' : author;
+    this.pages = pages === '' ? 'Unknown' : pages;
     this.isRead = isRead;
   }
 
@@ -18,7 +18,7 @@ class Library {
 
   addBook(book) {
     this.books.push(book);
-    ui.update(this.books)
+    ui.update(this.books);
   }
 
   handleRemoveBook(e, i) {
@@ -28,84 +28,89 @@ class Library {
 }
 
 class UI {
-  constructor() {
-    
-  }
+  constructor() {}
 
   update(books) {
-    const booksContainer = document.querySelector("#books-container");
+    const booksContainer = document.querySelector('#books-container');
     booksContainer.replaceChildren();
-  
+
     books.forEach((book, index) => this.appendBook(book, index));
   }
 
   appendBook(book, index) {
-    const bookContainer = document.querySelector("#books-container");
+    const bookContainer = document.querySelector('#books-container');
     const bookHTML = this.getBookHTML(book, index);
     bookContainer.appendChild(bookHTML);
   }
 
   getBookHTML(book, i) {
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book");
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
     bookDiv.dataset.key = i;
-  
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove--book--button");
-    removeButton.textContent = "✕";
-    removeButton.addEventListener("click", (e) => library.handleRemoveBook(e, i));
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove--book--button');
+    removeButton.textContent = '✕';
+    removeButton.addEventListener('click', (e) => library.handleRemoveBook(e, i));
     bookDiv.appendChild(removeButton);
-  
-    const title = document.createElement("p");
-    title.classList.add("book--title");
+
+    const title = document.createElement('p');
+    title.classList.add('book--title');
     title.textContent = book.title;
     bookDiv.appendChild(title);
-  
-    const author = document.createElement("p");
-    author.classList.add("book--author");
+
+    const author = document.createElement('p');
+    author.classList.add('book--author');
     author.textContent = book.author;
     bookDiv.appendChild(author);
-  
-    const pages = document.createElement("p");
-    pages.classList.add("book--pages");
-    pages.textContent = book.pages + " pages";
+
+    const pages = document.createElement('p');
+    pages.classList.add('book--pages');
+    pages.textContent = book.pages + ' pages';
     bookDiv.appendChild(pages);
-  
-    const readButton = document.createElement("button");
-    readButton.classList.add("book--read--button");
+
+    const readButton = document.createElement('button');
+    readButton.classList.add('book--read--button');
     if (book.isRead) {
-      readButton.classList.add("read");
-      readButton.textContent = "Read";
+      readButton.classList.add('read');
+      readButton.textContent = 'Read';
     } else {
-      readButton.classList.add("notread");
-      readButton.textContent = "Not read";
+      readButton.classList.add('notread');
+      readButton.textContent = 'Not read';
     }
-    readButton.addEventListener("click", (e) => ui.handleUpdateReadStatus(e, i));
+    readButton.addEventListener('click', (e) => ui.handleUpdateReadStatus(e, i));
     bookDiv.appendChild(readButton);
-  
+
     return bookDiv;
   }
 
   handleNewBookClick(e) {
-    const form = document.querySelector("#add-book-form");
-    form.style.display = "flex";
+    const form = document.querySelector('#add-book-form');
+    form.style.display = 'flex';
   }
 
   handleCloseFormClick(e) {
-    const form = document.querySelector("#add-book-form");
-    form.style.display = "none";
+    const form = document.querySelector('#add-book-form');
+    form.style.display = 'none';
   }
-  
+
   handleAddBook(e) {
     e.preventDefault();
-    const formData = new FormData(document.querySelector("#add-book-form"));
-    const title = formData.get("title");
-    const author = formData.get("author");
-    const pages = formData.get("pages");
-    const isRead = formData.get("isRead") ? true : false;
-  
+
+    const form = document.querySelector('#add-book-form');
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
+
+    const formData = new FormData(form);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const pages = formData.get('pages');
+    const isRead = formData.get('isRead') ? true : false;
+
     const book = new Book(title, author, pages, isRead);
-  
+
     library.addBook(book);
   }
 
@@ -115,20 +120,17 @@ class UI {
   }
 }
 
-
 const library = new Library();
 const ui = new UI();
 
+const newBookButton = document.querySelector('#add-new-book');
+newBookButton.addEventListener('click', ui.handleNewBookClick);
 
-const newBookButton = document.querySelector("#add-new-book");
-newBookButton.addEventListener("click", ui.handleNewBookClick);
+const closeFormButton = document.querySelector('#close--form--button');
+closeFormButton.addEventListener('click', ui.handleCloseFormClick);
 
-const closeFormButton = document.querySelector("#close--form--button");
-closeFormButton.addEventListener("click", ui.handleCloseFormClick);
-
-const addBook = document.querySelector("#add-book");
-addBook.addEventListener("click", ui.handleAddBook);
-
+const addBook = document.querySelector('#add-book');
+addBook.addEventListener('click', ui.handleAddBook);
 
 // HELPER FUNCTIONS
 
