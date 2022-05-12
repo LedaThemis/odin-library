f20dca41bdff23d3be1f91afbbaee1e95b0c290a
Leda <ledathemis@protonmail.com>
2022-05-12 08:30:40 +0000

    reformat

--- src/helpers/registerHandlers.js
+++ src/helpers/registerHandlers.js
@@ -1,11 +1,10 @@
 const registerHandlers = (ui) => {
   const newBookButton = document.querySelector('#add-new-book');
-  newBookButton.addEventListener('click', ui.handleNewBookClick);
-
   const closeFormButton = document.querySelector('#close--form--button');
-  closeFormButton.addEventListener('click', ui.handleCloseFormClick);
-
   const addBook = document.querySelector('#add-book');
+
+  newBookButton.addEventListener('click', ui.handleNewBookClick);
+  closeFormButton.addEventListener('click', ui.handleCloseFormClick);
   addBook.addEventListener('click', (e) => ui.handleAddBook(e));
 };
 
